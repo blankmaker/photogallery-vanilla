a4aafe98cee7ea70bd8cfa2149b73ce482b82a20
David Nguyen <davidnguyen84@gmail.com>
2016-02-11 14:26:04 -0800

    Separate building lightbox from loading lightbox functions

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ var images;
 
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('gallery').addEventListener('click', function(e) {
-    loadLightbox(e.target.dataset.id, e.target.dataset.position, images);
+    loadLightbox(e.target.dataset.position, images);
   });
 });
 
-function loadLightbox(imageIdString, imagePosition, imageCollection) {
+function loadLightbox(imagePosition, imageCollection) {
   var body = document.getElementsByTagName('body')[0];
   body.style.overflow = 'hidden';
 
-  var lightbox = document.getElementById('lightbox');
+  var lightbox = buildLightbox(imageCollection[imagePosition]);
+  lightbox.style.display = 'block';
+}
+
+function buildLightbox(imageObj) {
+  var background = document.getElementById('lightbox');
   var imageContainer = document.createElement('figure');
   var image = document.createElement('img');
   var imageCaption = document.createElement('figcaption');
@@ -20,16 +25,16 @@ function loadLightbox(imageIdString, imagePosition, imageCollection) {
   imageContainer.className = 'lightbox__imagecontainer';
   image.className = 'lightbox__image';
   imageCaption.className = 'lightbox__imagecaption';
-  imageCaption.innerHTML = imageCollection[imagePosition].title + 'by' + '<a href="http://www.google.com">me</a>';
 
-  image.src = imageCollection[imagePosition].url_c ? imageCollection[imagePosition].url_c : imageCollection[imagePosition].url_m;
+  imageCaption.innerHTML = '"' + imageObj.title + '"' +  ' by ' + '<a href="https://www.flickr.com/photos/' + imageObj.owner + '">' + imageObj.ownername + '</a>';
+  image.src = imageObj.url_c ? imageObj.url_c : imageObj.url_m;
 
   imageContainer.appendChild(image);
   imageContainer.appendChild(imageCaption);
-  lightbox.appendChild(imageContainer);
-  lightbox.style.display = 'block';
-}
+  background.appendChild(imageContainer);
 
+  return background;
+}
 
 function attachThumbnails(imagesCollection) {
   imagesCollection.forEach(function(imageObj, i) {
@@ -54,7 +59,7 @@ function makeThumbnail(imageObj, position) {
 // API Variables
 var apiKey = 'aec6f380f4286b7eb92b912e38445e48';
 var galleryId = '72157663691204050';
-var extras = 'description,date_taken,owner_name,url_q,url_c,url_m';
+var extras = 'owner_name,url_q,url_c,url_m';
 var format = 'json';
 var flickrUrl = 'https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos'
                   .concat('&api_key=', apiKey)
